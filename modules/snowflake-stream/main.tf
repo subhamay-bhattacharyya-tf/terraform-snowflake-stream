d5212530b9a84bfcf9cb3e6401038852c3058db7
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-02-08 08:14:00 -0500

    feat(snowflake-stream): use fully qualified table name in stream resource
    - Update table reference to use fully qualified name format
    - Change table parameter from `each.value.table` to `${each.value.database}.${each.value.schema}.${each.value.table}`
    - Ensures proper table identification in Snowflake stream configuration by including database and schema context

--- modules/snowflake-stream/main.tf
+++ modules/snowflake-stream/main.tf
@@ -7,7 +7,7 @@ resource "snowflake_stream_on_table" "this" {
   name              = each.value.name
   database          = each.value.database
   schema            = each.value.schema
-  table             = each.value.table
+  table             = "${each.value.database}.${each.value.schema}.${each.value.table}"
   append_only       = each.value.append_only
   show_initial_rows = each.value.show_initial_rows
   copy_grants       = each.value.copy_grants
